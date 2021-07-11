37a5d2d24d8ee9d8b52cd0154186fb883462eff7
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2021-07-11 18:42:17 +0900

    add: create integration test

--- server.js
+++ server.js
@@ -23,5 +23,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
+
+module.exports = app;
